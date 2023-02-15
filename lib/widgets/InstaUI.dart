5a51de8facea90b0885165395b3031ac096bff17
S A T Z O <sudosathish@gmail.com>
2023-02-15 05:38:42 +0530

    feb-15

--- lib/widgets/InstaUI.dart
+++ lib/widgets/InstaUI.dart
@@ -1,3 +1,5 @@
+//import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 class InstaUI extends StatelessWidget {
@@ -7,6 +9,7 @@ class InstaUI extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: false,
         leading: Icon(
           Icons.arrow_back_ios,
           color: Colors.black,
@@ -44,40 +47,186 @@ class InstaUI extends StatelessWidget {
                         ),
                         Text(
                           'Wanda S',
-                          style: TextStyle(fontSize: 20),
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
                         ),
                         SizedBox(
                           height: 2,
                         ),
                         Text(
-                          'Photographer/New york',
-                          style: TextStyle(fontSize: 14),
+                          'Photographer/Newyork',
+                          style: TextStyle(
+                              fontSize: 13,
+                              color: Colors.red,
+                              fontWeight: FontWeight.bold),
                         )
                       ],
                     ),
                   ),
                   Expanded(
                       child: Container(
-                    color: Colors.orange,
-                  ))
+                          //color: Colors.orange,
+                          child: Column(
+                    children: [
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            profileDetails('150', 'Posts'),
+                            profileDetails('5K', 'Followers'),
+                            profileDetails('100', 'Following'),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: Container(
+                                height: 40,
+                                child: ElevatedButton(
+                                    style: ButtonStyle(
+                                        shape: MaterialStateProperty.all(
+                                            RoundedRectangleBorder(
+                                                borderRadius:
+                                                    BorderRadius.circular(
+                                                        20)))),
+                                    onPressed: () {},
+                                    child: Text('Follow')),
+                              ),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Container(
+                              height: 40,
+                              width: 40,
+                              decoration: BoxDecoration(
+                                border:
+                                    Border.all(color: Colors.blue, width: 2.5),
+                                color: Colors.white,
+                                shape: BoxShape.circle,
+                              ),
+                              child: Icon(
+                                Icons.keyboard_arrow_down,
+                                color: Colors.blue,
+                              ),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  )))
                 ],
               ),
             ),
             Container(
-              height: 100,
-              color: Colors.blue,
+              height: 120,
+              //color: Colors.blue,
+              child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 5,
+                  itemBuilder: ((context, index) {
+                    return Column(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.all(5),
+                          height: 80,
+                          width: 80,
+                          decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  fit: BoxFit.cover,
+                                  image: NetworkImage(
+                                      'https://wallpapercave.com/wp/wp5575012.jpg')),
+                              shape: BoxShape.circle,
+                              color: Colors.orange),
+                        ),
+                        Text('Title')
+                      ],
+                    );
+                  })),
             ),
             Container(
-              height: 100,
-              color: Colors.green,
+              height: 60,
+              color: Colors.white,
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text('Email',
+                            style: TextStyle(
+                                fontSize: 15,
+                                color: Colors.blue,
+                                fontWeight: FontWeight.bold)),
+                        Text('Phone',
+                            style: TextStyle(
+                                fontSize: 15,
+                                color: Colors.blue,
+                                fontWeight: FontWeight.bold)),
+                        Text('Site',
+                            style: TextStyle(
+                                fontSize: 15,
+                                color: Colors.blue,
+                                fontWeight: FontWeight.bold))
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
             Expanded(
                 child: Container(
-              color: Colors.yellow,
+              //color: Colors.yellow,
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3),
+                itemBuilder: (context, index) {
+                  return Container(
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: NetworkImage(
+                                'https://i.pinimg.com/736x/bf/0c/25/bf0c2577c365b9430195ffe566ce1f10.jpg')),
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.grey.shade200),
+                    margin: EdgeInsets.all(5),
+                    height: 50,
+                    width: 50,
+                    //color: Colors.red,
+                  );
+                },
+              ),
             ))
           ],
         ),
       ),
+    );
+  }
+
+  Widget profileDetails(String text1, text2) {
+    return Column(
+      children: [
+        Text(
+          text1,
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        Text(
+          text2,
+          style: TextStyle(
+              fontSize: 14, color: Colors.red, fontWeight: FontWeight.bold),
+        )
+      ],
     );
   }
 }
